5e34b50b01cdbccc1caf50ed9f9e4d169c686fc8
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-01-24 16:43:06 +0100

    je pousse les premiers fichier sur le dépôt disant

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'Métodes.php';
+
+entrees();
---
+++ modificationcarte.php
@@ -0,0 +1,18 @@
+<?php
+/*
+//Création de la base de donnée:
+try{
+    $dsn = 'mysql:host=localhost';
+    $pdo = new PDO($dsn,'root','');
+    if($pdo->exec('CREATE DATABASE quaiantique')){
+        echo 'Le schéma a été créé';
+    }
+
+} catch(Exception $exception) {
+    echo 'une erreur c\'est produite:' . $exception->getCode();
+}
+*/
+
+
+
+
