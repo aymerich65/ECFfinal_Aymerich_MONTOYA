a1c66e27f8ecc2b1c1e4845cfb9c1c81e73a5858
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-01-27 10:54:21 +0100

    commit of index.php

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
-<?php
-require_once 'Métodes.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QuaiAntique</title>
+    <link rel="stylesheet" href="CSS/bootstrap.css">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-entrees();
+
+</head>
+<body class="bodystyle">
+
+<div class="container-fluid">
+    <div class="header row"> 
+       <?php require './templates/header.php';
+       ?>
+
+    </div>
+
+        <div class="main row">
+
+        </div>
+
+            <div class="footer row">
+                
+            </div>
+
+        
+                
+
+   
+
+</div>
+
+
+
+
+<script src="JS/bootstrap.bundle.min.js"></script>
+</body>
+</html>
