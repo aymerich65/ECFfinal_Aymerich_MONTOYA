09777784ab5efa77b5628c7e4c274e8f74124f91
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-03-24 17:00:36 +0100

    modification du fichier config : ajout de chaque variable env

--- config.php
+++ config.php
@@ -1,35 +1,15 @@
 <?php
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-if (getenv("JAWSDB_URL")) {
-    /* Pour exécuter l'application sur Heroku: */
-
-
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
-
-    $hostname = $dbparts['host'];
-    $db_name = ltrim($dbparts['path'], '/');
-    $db_user = $dbparts['user'];
-    $db_password = $dbparts['pass'];
-    $db_dsn = "mysql:host=$hostname;dbname=$db_name;charset=utf8mb4";
-
-
-
-
-} else {
-    /* Pour exécuter l'application en locale: */
-    $db_dsn = getenv("DB_DSN");
-    $db_user = getenv("DB_USER");
-    $db_password = getenv("DB_PASSWORD");
-}
+$hostname = getenv('DB_HOSTNAME');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+$db_dsn = "mysql:host=$hostname;dbname=$db_name;charset=utf8mb4";
 
 define("DB_DSN", $db_dsn);
 define("DB_USER", $db_user);
 define("DB_PASSWORD", $db_password);
 
 define("JWT_SECRET_KEY", getenv("JWT_SECRET_KEY"));
-
-?>
