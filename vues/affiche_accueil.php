e599b6ad75a122896802a1b1b3997537b20c5ade
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-03-24 20:31:54 +0100

    modification de la variable env user en username

--- vues/affiche_accueil.php
+++ vues/affiche_accueil.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 $dsn = $_ENV['DB_DSN'];
-$envuser = $_ENV['DB_USER'];
+$envuser = $_ENV['DB_USERNAME'];
 $envpassword = $_ENV['DB_PASSWORD'];
 
 
