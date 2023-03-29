346b2b33e9c32b4f0cf468afe0231c993c921d6b
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-03-29 10:56:15 +0200

    Correction des  chemins dans les requires_once du script de récupération des horaires

--- modeles/recuperations_donnees/script_recuperation_horaires.php
+++ modeles/recuperations_donnees/script_recuperation_horaires.php
@@ -1,8 +1,8 @@
 <?php
 
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../config.php';
 $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
 
 
