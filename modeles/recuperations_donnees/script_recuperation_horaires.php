b0e3a0cfd29d606619ebe376f27eb7c05ec5dd65
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-03-29 10:52:37 +0200

    Correction du script de récupération des variables d'environnement dans le code de récupération des horaires en base de donnée

--- modeles/recuperations_donnees/script_recuperation_horaires.php
+++ modeles/recuperations_donnees/script_recuperation_horaires.php
@@ -1,7 +1,5 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
