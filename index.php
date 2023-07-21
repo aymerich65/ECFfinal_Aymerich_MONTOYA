7cb359e0d0061d738c13c83068812335c0ee5606
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-07-21 07:18:12 +0200

    Ajout des gestionnaires généraux d'erreurs et d'exceptions

--- index.php
+++ index.php
@@ -5,6 +5,26 @@ ini_set('display_errors', '0');
 if(session_status() === PHP_SESSION_NONE){
     session_start();
 }
+
+session_start();
+
+set_exception_handler(function (Throwable $exception) {
+    error_log($exception);
+    echo 'Une erreur technique s\'est produite. Veuillez réessayer plus tard.';
+});
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    error_log("Erreur PHP: [$errno] $errstr dans le fichier $errfile à la ligne $errline");
+    echo 'Une erreur s\'est produite. Veuillez réessayer plus tard.';
+});
+
+
+
+
+
+
+
+
 require_once 'controleurs/controles.php';
 //require_once 'JWT/verificationCookieJwt.php';
 
